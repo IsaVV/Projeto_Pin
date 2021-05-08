be7fbd42dd410f4fb5deecb83ebabf38b20b7da1
Gustavo Lima <gustavolvma@gmail.com>
2021-05-08 13:54:34 -0300

    Update

---
+++ pin/src/main/java/com/projeto/pin/causa/Causa.java
@@ -0,0 +1,7 @@
+package com.projeto.pin.causa;
+
+public class Causa {
+    private String tipo;
+    private String atributo;
+    private String disfuncao;
+}
---
+++ pin/src/main/java/com/projeto/pin/equipamento/Equipamento.java
@@ -0,0 +1,8 @@
+package com.projeto.pin.equipamento;
+
+public class Equipamento {
+    private String nome;
+    private String local;
+    private String tipo;
+
+}
