8281077d95acd83b03e1f1db1a2957de3d7e2b51
Isadora Vasconcelos Viana <64534791+IsaVV@users.noreply.github.com>
2021-05-08 14:10:57 -0300

    Add files via upload

---
+++ pin/src/main/java/com/projeto/pin/Descricao/descricao.java
@@ -0,0 +1,14 @@
+package com.projeto.pin;
+
+public class descricao {
+
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
