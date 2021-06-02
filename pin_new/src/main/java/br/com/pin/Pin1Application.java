eab0e7d80c109683edbcd640508c08503933f70c
Gustavo Lima <76820333+gustavolimav@users.noreply.github.com>
2021-06-02 18:56:16 -0300

    new file

---
+++ pin_new/src/main/java/br/com/pin/Pin1Application.java
@@ -0,0 +1,13 @@
+package br.com.pin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pin1Application.class, args);
+	}
+
+}
