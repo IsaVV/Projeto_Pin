1e354fedf3b87180354988d2e4d60c6ff7f61c97
Gustavo Lima <gustavolvma@gmail.com>
2021-06-02 19:45:23 -0300

    delete pin_new

--- pin_new/src/main/java/br/com/pin/controllers/DuracaoMinima.java
+++
@@ -1,5 +0,0 @@
-package br.com.pin.controllers;
-
-public class DuracaoMinima {
-    
-}
--- pin_new/src/main/java/br/com/pin/repository/RegistroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.pin.controllers.RegistroParada;
-
-public interface RegistroRepository extends JpaRepository<RegistroParada, Long>{
-    
-}
