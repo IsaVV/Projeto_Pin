743f8ec448c097357fdeda268218c20ed4cac1ff
Gustavo Lima <gustavolvma@gmail.com>
2021-06-03 21:29:34 -0300

    Refactor

---
+++ pin/src/main/java/br/com/pin/PinApplication.java
@@ -0,0 +1,12 @@
+package br.com.pin;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class PinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinApplication.class, args);
+	}
+
+}
